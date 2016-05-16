1fe654280ff3a54fd103be4ab8882943467f5763
shady <knightdawner@gmail.com>
2016-05-16 14:47:50 +0800

    Baic view now returns minamal HTML

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('Hello')
+    return HttpResponse("""<html><head><title>To-Do lists</title></head>
+                    <body><h1>This is a to-do list</h1></body>
+                    </html>""")
