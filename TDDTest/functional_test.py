052789896e36e937b1b8b472701ff696a503baf1
shady <knightdawner@gmail.com>
2016-05-16 17:01:15 +0800

    Refactor the functional text with the helper function

--- TDDTest/functional_test.py
+++ TDDTest/functional_test.py
@@ -18,6 +18,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
@@ -38,24 +43,28 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
         #self.assertTrue(
         #            any(row.text == '1: Buy peacock feathers' for row in rows),
         #            "New to-do item did not appear in table -- its text was: \n%s" % (table.text,)
         #        )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # The second test
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Use peacock feathers to make a fly")
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_elements_by_tag_name('tr')
+        #self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
 
         self.fail('Finish the test!')
+
+
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
