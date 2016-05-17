fed681ba39cb8c0ef86b8c5bcd897f185499e2ea
shady <knightdawner@gmail.com>
2016-05-17 14:33:58 +0800

    new URL + view for adding existing lists. FT passes :-)

--- lists/urls.py
+++ lists/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import view_list, new_list
+from .views import view_list, new_list, add_item
 
 urlpatterns = [
-        url(r'lists/the-only-list-in-the-world/$', view_list, name='view_list'),
+        url(r'lists/(\d+)/$', view_list, name='view_list'),
+        url(r'lists/(\d+)/add_item$', add_item, name='add_item'),
         url(r'lists/new$', new_list, name='new_list'),
         ]
 
