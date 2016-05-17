17614241ef175f3f9ffc1b30ac6b6c4eb677a975
shady <knightdawner@gmail.com>
2016-05-17 14:44:44 +0800

    A little refactor in urls django

--- lists/urls.py
+++ lists/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from .views import view_list, new_list, add_item
 
 urlpatterns = [
-        url(r'lists/(\d+)/$', view_list, name='view_list'),
-        url(r'lists/(\d+)/add_item$', add_item, name='add_item'),
-        url(r'lists/new$', new_list, name='new_list'),
+        url(r'^(\d+)/$', view_list, name='view_list'),
+        url(r'^(\d+)/add_item/$', add_item, name='add_item'),
+        url(r'^new/$', new_list, name='new_list'),
         ]
 
