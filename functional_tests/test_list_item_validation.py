0a40c97f38e0e1e499123a2d4ec6b749cb37f76e
shady <knightdawner@gmail.com>
2016-05-20 18:14:11 +0800

    Refactor list view to handle new item POSTs

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,23 +1,26 @@
 #from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import unittest
+import time
 from unittest import skip
 from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.server_url)
-        self.browser.find_element_by_css_selector('.has-error')
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # She tries again with some text for the item
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk')
 
-        self.find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
         # She recieve a similar warning on the list page
-        self.bowser.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('1: Buy milk')
         self.browser.find_element_by_css_selector('.has-error')
+        error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correc it by filling some text in
--- lists/views.py
+++ lists/views.py
@@ -14,6 +14,16 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        item = Item(text=request.POST['item_text'], list=list_)
+        try:
+            item.full_clean()
+            item.save()
+        except ValidationError:
+            error = "You can't have an empty list item"
+            return render(request, 'list.html', {'list': list_, 'error': error})
+            #return redirect('/lists/%d/' % (list_.id,), error=error)
+        return redirect('/lists/%d/' % (list_.id,))
     return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
@@ -29,9 +39,21 @@ def new_list(request):
     return  redirect('/lists/%d/' % (list_.id))
 
 def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
+    if request.method == 'POST':
+        list_ = List.objects.get(id=list_id)
+        item = Item(list=list_, text=request.POST['item_text'])
+        #Item.objects.create(text=request.POST['item_text'], list=list_)
+        try:
+            item.full_clean()
+            item.save()
+        except ValidationError:
+            error = "You can't have an empty list item"
+            return render(request, 'list.html', {'list': list_, 'error': error})
+            #return redirect('/lists/%d/' % (list_.id,), error=error)
+        return redirect('/lists/%d/' % (list_.id,))
+    else:
+        return redirect('/lists/%d/' % (list_.id,))
+
 
 
 
