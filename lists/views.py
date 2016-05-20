d03d085a7f28fa69cf6435dd952ff03a04ac7434
shady <knightdawner@gmail.com>
2016-05-20 18:57:00 +0800

    backup

--- lists/views.py
+++ lists/views.py
@@ -29,14 +29,14 @@ def view_list(request, list_id):
 def new_list(request):
     list_ = List.objects.create()
     item = Item(text=request.POST['item_text'], list=list_)
+    error = None
     try:
         item.full_clean()
         item.save()
     except ValidationError:
         list_.delete()
         error = 'You can\'t have an empty list item'
-        return render(request, 'home.html', {'error': error})
-    return  redirect('/lists/%d/' % (list_.id))
+    return render(request, 'home.html', {'error': error})
 
 def add_item(request, list_id):
     if request.method == 'POST':
